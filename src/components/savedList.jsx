e4e4fc1188a2ad5190389f3ddad7d8cb828becf1
BarackAdida <adida.barack@student.moringaschool.com>
2025-06-02 19:45:26 +0300

    Data display

---
+++ src/components/savedList.jsx
@@ -0,0 +1,12 @@
+// import { useEffect, useState } from "react";
+
+
+
+function SavedList() {
+    return (
+        <div>
+             <h1>Registered members</h1>
+        </div>
+    )
+}
+export default SavedList
